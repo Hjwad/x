545a18c16d1491508b5ac0a98020178c86d8d633
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-05 17:09:58 +0300

    Update Youtube.py

--- ZelzalMusic/platforms/Youtube.py
+++ ZelzalMusic/platforms/Youtube.py
@@ -52,12 +52,13 @@ class YouTubeAPI:
 
 
     async def exists(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if re.search(self.regex, link):
-            return True
-        else:
-            return False
+    if videoid:
+        link = self.base + link
+    if re.search(self.regex, link):
+        response = await self.session.get(link)   
+        return True if response.status_code == 200 else False
+    else:
+        return False
 
     async def url(self, message_1: Message) -> Union[str, None]:
         messages = [message_1]
@@ -82,6 +83,7 @@ class YouTubeAPI:
         if offset in (None,):
             return None
         return text[offset : offset + length]
+        response = await self.session.get(link)
 
     async def details(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
@@ -99,148 +101,180 @@ class YouTubeAPI:
             else:
                 duration_sec = int(time_to_seconds(duration_min))
         return title, duration_min, duration_sec, thumbnail, vidid
+        response = await self.session.get(link)
 
-    async def title(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            title = result["title"]
-        return title
+async def title(self, link: str, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    results = VideosSearch(link, limit=1)
+    for result in (await results.next())["result"]:
+        title = result["title"]
 
-    async def duration(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            duration = result["duration"]
-        return duration
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
 
-    async def thumbnail(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
+    return title
 
-    async def video(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        proc = await asyncio.create_subprocess_exec(
-            "yt-dlp",
-            "-g",
-            "-f",
-            "best[height<=?720][width<=?1280]",
-            f"{link}",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await proc.communicate()
-        if stdout:
-            return 1, stdout.decode().split("\n")[0]
-        else:
-            return 0, stderr.decode()
+async def duration(self, link: str, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    results = VideosSearch(link, limit=1)
+    for result in (await results.next())["result"]:
+        duration = result["duration"]
 
-    async def playlist(self, link, limit, user_id, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.listbase + link
-        if "&" in link:
-            link = link.split("&")[0]
-        playlist = await shell_cmd(
-            f"yt-dlp -i --get-id --flat-playlist --playlist-end {limit} --skip-download {link}"
-        )
-        try:
-            result = playlist.split("\n")
-            for key in result:
-                if key == "":
-                    result.remove(key)
-        except:
-            result = []
-        return result
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
 
-    async def track(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            title = result["title"]
-            duration_min = result["duration"]
-            vidid = result["id"]
-            yturl = result["link"]
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        track_details = {
-            "title": title,
-            "link": yturl,
-            "vidid": vidid,
-            "duration_min": duration_min,
-            "thumb": thumbnail,
-        }
-        return track_details, vidid
+    return duration
+        
 
-    async def formats(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        ytdl_opts = {"quiet": True}
-        ydl = yt_dlp.YoutubeDL(ytdl_opts)
-        with ydl:
-            formats_available = []
-            r = ydl.extract_info(link, download=False)
-            for format in r["formats"]:
+async def thumbnail(self, link: str, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    results = VideosSearch(link, limit=1)
+    for result in (await results.next())["result"]:
+        thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
+
+    return thumbnail
+async def video(self, link: str, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    proc = await asyncio.create_subprocess_exec(
+        "yt-dlp",
+        "-g",
+        "-f",
+        "best[height<=?720][width<=?1280]",
+        f"{link}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        # إضافة السطر المطلوب
+        response = await self.session.get(link)
+        
+        return 1, stdout.decode().split("\n")[0]
+    else:
+        return 0, stderr.decode()
+
+async def playlist(self, link, limit, user_id, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.listbase + link
+    if "&" in link:
+        link = link.split("&")[0]
+    playlist = await shell_cmd(
+        f"yt-dlp -i --get-id --flat-playlist --playlist-end {limit} --skip-download {link}"
+    )
+    try:
+        result = playlist.split("\n")
+        for key in result:
+            if key == "":
+                result.remove(key)
+    except:
+        result = []
+
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
+
+    return result
+
+async def track(self, link: str, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    results = VideosSearch(link, limit=1)
+    for result in (await results.next())["result"]:
+        title = result["title"]
+        duration_min = result["duration"]
+        vidid = result["id"]
+        yturl = result["link"]
+        thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+    track_details = {
+        "title": title,
+        "link": yturl,
+        "vidid": vidid,
+        "duration_min": duration_min,
+        "thumb": thumbnail,
+    }
+
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
+
+    return track_details, vidid
+
+async def formats(self, link: str, videoid: Union[bool, str] = None):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    ytdl_opts = {"quiet": True}
+    ydl = yt_dlp.YoutubeDL(ytdl_opts)
+    with ydl:
+        formats_available = []
+        r = ydl.extract_info(link, download=False)
+        for format in r["formats"]:
+            try:
+                str(format["format"])
+            except:
+                continue
+            if not "dash" in str(format["format"]).lower():
                 try:
-                    str(format["format"])
+                    format["format"]
+                    format["filesize"]
+                    format["format_id"]
+                    format["ext"]
+                    format["format_note"]
                 except:
                     continue
-                if not "dash" in str(format["format"]).lower():
-                    try:
-                        format["format"]
-                        format["filesize"]
-                        format["format_id"]
-                        format["ext"]
-                        format["format_note"]
-                    except:
-                        continue
-                    formats_available.append(
-                        {
-                            "format": format["format"],
-                            "filesize": format["filesize"],
-                            "format_id": format["format_id"],
-                            "ext": format["ext"],
-                            "format_note": format["format_note"],
-                            "yturl": link,
-                        }
-                    )
-        return formats_available, link
+                formats_available.append(
+                    {
+                        "format": format["format"],
+                        "filesize": format["filesize"],
+                        "format_id": format["format_id"],
+                        "ext": format["ext"],
+                        "format_note": format["format_note"],
+                        "yturl": link,
+                    }
+                )
 
-    async def slider(
-        self,
-        link: str,
-        query_type: int,
-        videoid: Union[bool, str] = None,
-    ):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        a = VideosSearch(link, limit=10)
-        result = (await a.next()).get("result")
-        title = result[query_type]["title"]
-        duration_min = result[query_type]["duration"]
-        vidid = result[query_type]["id"]
-        thumbnail = result[query_type]["thumbnails"][0]["url"].split("?")[0]
-        return title, duration_min, thumbnail, vidid
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
+
+    return formats_available, link
+
+async def slider(
+    self,
+    link: str,
+    query_type: int,
+    videoid: Union[bool, str] = None,
+):
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+    a = VideosSearch(link, limit=10)
+    result = (await a.next()).get("result")
+    title = result[query_type]["title"]
+    duration_min = result[query_type]["duration"]
+    vidid = result[query_type]["id"]
+    thumbnail = result[query_type]["thumbnails"][0]["url"].split("?")[0]
+
+    # إضافة السطر المطلوب
+    response = await self.session.get(link)
+
+    return title, duration_min, thumbnail, vidid
 
     async def download(
         self,
