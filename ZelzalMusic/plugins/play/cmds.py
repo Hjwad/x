ccdc47bcff3d2041e97e21245b25cf5fb725c556
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-08 00:48:01 +0300

    Update cmds.py

--- ZelzalMusic/plugins/play/cmds.py
+++ ZelzalMusic/plugins/play/cmds.py
@@ -46,15 +46,19 @@ async def zdatsr(client: Client, message: Message):
     )
 
 
-@app.on_message(command(["مطور", "المطور"]) & filters.group)
+
+@app.on_message(filters.command(["مطور", "المطور"]) & filters.group)
 async def zilzal(client: Client, message: Message):
     usr = await client.get_users(OWNER_ID)
     name = usr.first_name
     usrnam = usr.username
-    async for photo in client.set_profile_photo(OWNER_ID, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""ٴ<b>•────‌‌‏✯ ᴇ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
+    
+    photos = await client.get_profile_photos(OWNER_ID, limit=1)
+    if photos:
+        photo = photos[0]
+        await message.reply_photo(photo.file_id, caption=f"""ٴ<b>•────‌‌‏✯ ᴇ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
                     
-- المطور :[{usr.first_name}](https://t.me/{OWNER_ID})
+- المطور :[{usr.first_name}](https://t.me/{usrnam})
 - اليوزر :@{usrnam} 
 - الايدي :`{usr.id}`
  
@@ -68,10 +72,12 @@ reply_markup=InlineKeyboardMarkup(
             ],
           ] 
        )                 
-    )                    
-                    sender_id = message.from_user.id
-                    sender_name = message.from_user.first_name
-                    senderuser = message.from_user.username
-                    sender_user = "@{senderuser}" if senderuser else "لا يوجـد"
-                    await app.send_message(OWNER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
-                    return await app.send_message(LOGGER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
+    )
+    
+    sender_id = message.from_user.id
+    sender_name = message.from_user.first_name
+    senderuser = message.from_user.username
+    sender_user = f"@{senderuser}" if senderuser else "لا يوجـد"
+    
+    await app.send_message(OWNER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
+    await app.send_message(LOGGER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
